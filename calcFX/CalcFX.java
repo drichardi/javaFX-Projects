846e66369027ed3faffd26c14ccd34a1e9b93e51
d_richardi <d_richardi@sau9.org>
2016-11-15 20:39:37 -0500

    Added styling

--- calcFX/CalcFX.java
+++ calcFX/CalcFX.java
@@ -12,57 +12,158 @@ import javafx.stage.Stage;
 
 public class CalcFX extends Application
 {
-  TextField txtnum1, txtnum2;
-  Button btnadd, btnsub, btndiv, btnmul, btnclear;
-  Label lblanswer;
+  Button btnAdd, btnSub, btnDiv, btnMul, btnAC, btnRad, btnInv, btnPi, btnE, btnAns, btnSin, btnCos, btnTan, btnEXP, btnDeg, btnFactorial, btnLn, btnLog, btnSqRoot, btnXy, btnLParen, btnRParen, btnPercent, btnNum0, btnNum1, btnNum2, btnNum3, btnNum4, btnNum5, btnNum6, btnNum7, btnNum8, btnNum9, btnDecimalPoint, btnEquals;
+  Label lblAnswer, lblInput;
+
+
 
   @Override
   public void start(Stage theStage)
   {
     //make controls
-    txtnum1 = new TextField();
-    txtnum2 = new TextField();
-    btnadd = new Button("+");
-    btnsub = new Button("-");
-    btnmul = new Button("x");
-    btndiv = new Button("/");
-    btnclear = new Button("Clear");
-    lblanswer = new Label("?");
+    lblAnswer = new Label("");
+    lblInput = new Label("");
+    btnRad = new Button("Rad");
+    btnInv = new Button("Inv");
+    btnPi = new Button("Pi");
+    btnE = new Button("e");
+    btnAns = new Button("Ans");
+    btnDeg = new Button("Deg");
+    btnSin = new Button("sin");
+    btnCos = new Button("cos");
+    btnTan = new Button("tan");
+    btnEXP = new Button("EXP");
+    btnFactorial = new Button("x!");
+    btnLn = new Button("ln");
+    btnLog = new Button("log");
+    btnSqRoot = new Button("\u221A");
+    btnXy = new Button("xY");
+    btnLParen = new Button("(");
+    btnRParen = new Button(")");
+    btnAdd = new Button("+");
+    btnSub = new Button("-");
+    btnMul = new Button("x");
+    btnDiv = new Button("/");
+    btnAC = new Button("AC");
+    btnPercent = new Button("%");
+    btnEquals = new Button("=");
+    btnDecimalPoint = new Button(".");
+    btnNum0 = new Button("0");
+    btnNum1 = new Button("1");
+    btnNum2 = new Button("2");
+    btnNum3 = new Button("3");
+    btnNum4 = new Button("4");
+    btnNum5 = new Button("5");
+    btnNum6 = new Button("6");
+    btnNum7 = new Button("7");
+    btnNum8 = new Button("8");
+    btnNum9 = new Button("9");
 
-    //center text in Label
-    lblanswer.setAlignment(Pos.CENTER);
+
+    //right align text in Label
+    //lblAnswer.setAlignment(Pos.RIGHT);
     //apply css-like style to Label
 
-    lblanswer.setStyle("-fx-border-color: #000; -fx-padding: 5px;");
+  //  lblanswer.setStyle("-fx-border-color: #000; -fx-padding: 5px;");
 
     //make container for app
+
     GridPane root = new GridPane();
     //put container in middle of scene
     root.setAlignment(Pos.CENTER);
     //set spacing between controls
-    root.setHgap(10);
-    root.setVgap(10);
+    root.setHgap(4);
+    root.setVgap(4);
     //add to grid, cell by cell
-    root.add(btnadd, 0,0);
-    root.add(btnsub, 1,0);
-    root.add(btnmul, 0,1);
-    root.add(btndiv, 1,1);
-    root.add(txtnum1, 0,2);
-    root.add(txtnum2, 1,2);
+    //answer
+    root.add(lblAnswer, 0, 0, 7, 1);
+    //input
+    root.add(lblInput, 0, 1, 7, 1);
+    //row 1
+    root.add(btnRad, 0,2);
+    root.add(btnDeg, 1,2);
+    root.add(btnFactorial, 2,2);
+    root.add(btnLParen, 3,2);
+    root.add(btnRParen, 4,2);
+    root.add(btnPercent, 5,2);
+    root.add(btnAC, 6, 2);
+    //row 2
+    root.add(btnInv, 0,3);
+    root.add(btnSin, 1, 3);
+    root.add(btnLn, 2, 3);
+    root.add(btnNum7, 3, 3);
+    root.add(btnNum8, 4, 3);
+    root.add(btnNum9, 5, 3);
+    root.add(btnDiv, 6, 3);
+    //row 3
+    root.add(btnPi, 0, 4);
+    root.add(btnCos, 1, 4);
+    root.add(btnLog, 2, 4);
+    root.add(btnNum4, 3, 4);
+    root.add(btnNum5, 4, 4);
+    root.add(btnNum6, 5, 4);
+    root.add(btnMul, 6, 4);
+    //row 4
+    root.add(btnE, 0, 5);
+    root.add(btnTan, 1, 5);
+    root.add(btnSqRoot, 2, 5);
+    root.add(btnNum1, 3, 5);
+    root.add(btnNum2, 4, 5);
+    root.add(btnNum3, 5, 5);
+    root.add(btnSub, 6, 5);
+    //row 5
+    root.add(btnAns, 0, 6);
+    root.add(btnEXP, 1, 6);
+    root.add(btnXy, 2, 6);
+    root.add(btnNum0, 3, 6);
+    root.add(btnDecimalPoint, 4, 6);
+    root.add(btnEquals, 5, 6);
+    root.add(btnAdd, 6, 6);
+
+
     //last 2 rows span across 2 columns
     //col, row, colspan, rowspan
-    root.add(lblanswer, 0, 3, 2, 1);
-    root.add(btnclear, 0, 4, 2, 1);
+    //root.add(lblanswer, 0, 3, 2, 1);
+    //root.add(btnclear, 0, 4, 2, 1);
     //set widths of all controls in method
+    root.getChildren().forEach(node ->
+      node.setStyle("-fx-min-width: 60px; -fx-min-height: 40px; -fx-background-color: #ccc;")
+    );
+    lblInput.setStyle("-fx-min-width: 445px; -fx-padding: 10px; -fx-min-height: 60px; -fx-background-color: #000;");
+    lblAnswer.setStyle("-fx-min-width: 445px; -fx-height: 20px; -fx-min-height: 20px; -fx-padding: 5px; -fx-background-color: #888;");
+    btnEquals.setStyle("-fx-min-width: 60px; -fx-min-height: 40px; -fx-background-color: #298edb;");
+    {
+      String styleNumPad = btnNum0.getStyle();
+      styleNumPad += "-fx-background-color: #ddd;";
+      btnNum0.setStyle(styleNumPad);
+      btnNum1.setStyle(styleNumPad);
+      btnNum2.setStyle(styleNumPad);
+      btnNum3.setStyle(styleNumPad);
+      btnNum4.setStyle(styleNumPad);
+      btnNum5.setStyle(styleNumPad);
+      btnNum6.setStyle(styleNumPad);
+      btnNum7.setStyle(styleNumPad);
+      btnNum8.setStyle(styleNumPad);
+      btnNum9.setStyle(styleNumPad);
+      btnDecimalPoint.setStyle(styleNumPad);
+    }
+
+
+
+    /*
     setWidths();
     //attach buttons to code in method
     linkButtonFunctions();
     //usual stuff for apps
-    Scene scene = new Scene(root, 300, 250);
+    */
+
+    Scene scene = new Scene(root, 600, 400);
     theStage.setTitle("Mathemagic 1.0");
     theStage.setScene(scene);
     theStage.show();
   }
+
+  /*
     public void setWidths()
     {
       //set widths of all controls
@@ -125,6 +226,7 @@ public class CalcFX extends Application
       lblanswer.setText("" + num1 + symbol + num2 + " = " + answer);
 
     }
+    */
     public static void main(String[] args) {
       launch(args);
     }
